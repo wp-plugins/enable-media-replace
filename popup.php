a598627cf3b27ef7f9a5f2989f865b601156e15e
MungoBBQ <MungoBBQ@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-02-18 14:54:37 +0000

    Tagging 1.4
    git-svn-id: https://plugins.svn.wordpress.org/enable-media-replace/tags/1.4@207607 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- popup.php
+++ popup.php
@@ -23,35 +23,35 @@ $current_filename = substr($current_filename, (strrpos($current_filename, "/") +
 
 ?><html>
 <head>
-	<title><?=__("Replace media upload")?></title>
+	<title><?php echo __("Replace media upload"); ?></title>
 	
-<link rel='stylesheet' href='<?=get_bloginfo("wpurl");?>/wp-admin/css/global.css?ver=20081210' type='text/css' media='all' />
-<link rel='stylesheet' href='<?=get_bloginfo("wpurl");?>/wp-admin/wp-admin.css?ver=20081210' type='text/css' media='all' />
-<link rel='stylesheet' href='<?=get_bloginfo("wpurl");?>/wp-admin/css/colors-fresh.css?ver=20081210' type='text/css' media='all' />
-<link rel='stylesheet' href='<?=get_bloginfo("wpurl");?>/wp-admin/css/media.css?ver=20081210' type='text/css' media='all' />
+<link rel='stylesheet' href='<?php echo get_bloginfo("wpurl");?>/wp-admin/css/global.css?ver=20081210' type='text/css' media='all' />
+<link rel='stylesheet' href='<?php echo get_bloginfo("wpurl");?>/wp-admin/wp-admin.css?ver=20081210' type='text/css' media='all' />
+<link rel='stylesheet' href='<?php echo get_bloginfo("wpurl");?>/wp-admin/css/colors-fresh.css?ver=20081210' type='text/css' media='all' />
+<link rel='stylesheet' href='<?php echo get_bloginfo("wpurl");?>/wp-admin/css/media.css?ver=20081210' type='text/css' media='all' />
 </head>
 <body id="media-upload">
 <div class="wrap">
 		<div id="icon-upload" class="icon32"><br /></div>
-	<h2><?=__("Replace Media Upload", "enable-media-replace")?></h2>
+	<h2><?php echo __("Replace Media Upload", "enable-media-replace"); ?></h2>
 	
-	<form enctype="multipart/form-data" method="post" action="<?=get_bloginfo("wpurl") . "/wp-content/plugins/enable-media-replace/upload.php"?>">
-		<input type="hidden" name="ID" value="<?=$_GET["attachment_id"]?>" />
-		<div id="message" class="updated fade"><p><?=__("NOTE: You are about to replace the media file", "enable-media-replace")?> "<?=$current_filename?>". <?=__("There is no undo. Think about it!", "enable-media-replace")?></p></div>
+	<form enctype="multipart/form-data" method="post" action="<?php echo get_bloginfo("wpurl") . "/wp-content/plugins/enable-media-replace/upload.php"; ?>">
+		<input type="hidden" name="ID" value="<?php echo $_GET["attachment_id"]; ?>" />
+		<div id="message" class="updated fade"><p><?php echo __("NOTE: You are about to replace the media file", "enable-media-replace"); ?> "<?php echo $current_filename?>". <?php echo __("There is no undo. Think about it!", "enable-media-replace"); ?></p></div>
 	
-		<p><?=__("Choose a file to upload from your computer", "enable-media-replace")?></p>
+		<p><?php echo __("Choose a file to upload from your computer", "enable-media-replace"); ?></p>
 	
 		<input type="file" name="userfile" />
 		
-		<p><?=__("Select media replacement type:", "enable-media-replace")?></p>
+		<p><?php echo __("Select media replacement type:", "enable-media-replace"); ?></p>
 		
-		<label for="replace_type_1"><input CHECKED id="replace_type_1" type="radio" name="replace_type" value="replace"> <?=__("Just replace the file", "enable-media-replace")?></label>
-		<p class="howto"><?=__("Note: This option requires you to upload a file of the same type (", "enable-media-replace")?><?=$current_filetype?><?=__(") as the one you are replacing. The name of the attachment will stay the same (", "enable-media-replace")?><?=$current_filename?><?=__(") no matter what the file you upload is called.", "enable-media-replace")?></p>
+		<label for="replace_type_1"><input CHECKED id="replace_type_1" type="radio" name="replace_type" value="replace"> <?php echo __("Just replace the file", "enable-media-replace"); ?></label>
+		<p class="howto"><?php echo __("Note: This option requires you to upload a file of the same type (", "enable-media-replace"); ?><?php echo $current_filetype; ?><?php echo __(") as the one you are replacing. The name of the attachment will stay the same (", "enable-media-replace"); ?><?php echo $current_filename; ?><?php echo __(") no matter what the file you upload is called.", "enable-media-replace"); ?></p>
 		
-		<label for="replace_type_2"><input id="replace_type_2" type="radio" name="replace_type" value="replace_and_search"> <?=__("Replace the file, use new file name and update all links", "enable-media-replace")?></label>
-		<p class="howto"><?=__("Note: If you check this option, the name and type of the file you are about to upload will replace the old file. All links pointing to the current file (", "enable-media-replace")?><?=$current_filename?><?=__(") will be updated to point to the new file name.", "enable-media-replace")?></p>
+		<label for="replace_type_2"><input id="replace_type_2" type="radio" name="replace_type" value="replace_and_search"> <?php echo __("Replace the file, use new file name and update all links", "enable-media-replace"); ?></label>
+		<p class="howto"><?php echo __("Note: If you check this option, the name and type of the file you are about to upload will replace the old file. All links pointing to the current file (", "enable-media-replace"); ?><?php echo $current_filename; ?><?php echo __(") will be updated to point to the new file name.", "enable-media-replace"); ?></p>
 		
-		<input type="submit" class="button" value="<?=__("Upload", "enable-media-replace")?>" /> <a href="#" onclick="window.close();"><?=__("Cancel", "enable-media-replace")?></a>
+		<input type="submit" class="button" value="<?php echo __("Upload", "enable-media-replace"); ?>" /> <a href="#" onclick="window.close();"><?php echo __("Cancel", "enable-media-replace"); ?></a>
 
 	</form>
 </div>
