f95e20f9c267b96a7a4ee0a1494b6d01ea147f32
MungoBBQ <MungoBBQ@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-23 07:30:17 +0000

    Tagging 2.5.1
    git-svn-id: https://plugins.svn.wordpress.org/enable-media-replace/tags/2.5.1@442503 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- enable-media-replace.php
+++ enable-media-replace.php
@@ -3,7 +3,7 @@
 Plugin Name: Enable Media Replace
 Plugin URI: http://www.mansjonasson.se/enable-media-replace
 Description: Enable replacing media files by uploading a new file in the "Edit Media" section of the WordPress Media Library.
-Version: 2.5
+Version: 2.5.1
 Author: Måns Jonasson
 Author URI: http://www.mansjonasson.se
 
